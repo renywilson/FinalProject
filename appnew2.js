7966f63143e19034c5ecb8c20f0a3a64f4421549
renywilson <116873667+renywilson@users.noreply.github.com>
2023-02-03 16:04:59 +0530

    shop forms

--- appnew2.js
+++ appnew2.js
@@ -1,14 +1,19 @@
 const express=require('express');//imported
 const bodyParser=require('body-parser');
 const app=express();
+const path = require('path');
 const adminRoutes=require('./Routers/admin');
 const shopRoutes=require('./Routers/shop');
+const contactRoutes=require('./Routers/Contactus');
+const sucessRoutes=require('./Routers/sucess');
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(contactRoutes);
+app.use(sucessRoutes);
 app.use('/admin',adminRoutes);
-app.use('/shop',shopRoutes);
+app.use(shopRoutes);
 
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
 
-app.listen(3000);
+app.listen(5000);
